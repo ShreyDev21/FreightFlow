c9ac22fe7ba6fd11b85ca99e232adce36cf2a409
shreyash-porwal-sttl <shreyash.porwal@silvertouch.com>
2026-03-25 17:03:57 +0530

    feat(shipment): add EF Core migrations - create shipments table

---
+++ backend/services/Shipment/FreightFlow.Shipment.Infrastructure/Persistence/Migrations/20260325112917_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreightFlow.Shipment.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "shipments",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tracking_code = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    sender_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    receiver_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    origin_address = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    destination_address = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    weight_kg = table.Column<decimal>(type: "numeric(10,2)", precision: 10, scale: 2, nullable: false),
+                    status = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_shipments", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_shipments_tracking_code",
+                table: "shipments",
+                column: "tracking_code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "shipments");
+        }
+    }
+}
